45e5d89a5478225ff48bb8812786c53a46b50227
RahulRj09 <rahul18@navgurukul.org>
2019-05-30 15:16:18 +0530

    add. all type swap function

--- src/main/java/number/SwapNumber.java
+++ src/main/java/number/SwapNumber.java
@@ -5,26 +5,19 @@ import java.util.Arrays;
 import java.util.List;
 
 public class SwapNumber {
-    private static List<Integer> swapNumber(List<Integer> numbers, int a, int b) {
-        List<Integer> copyNumbers = new ArrayList<>(numbers);
-        int indexValue = copyNumbers.get(b);
-        copyNumbers.set(b, copyNumbers.get(a));
-        copyNumbers.set(a, indexValue);
-        return copyNumbers;
-    }
-     private static List<String> swapString(List<String> names, int a, int b) {
-        List<String> copyName = new ArrayList<>(names);
-        String indexValue = copyName.get(b);
-        names.set(b, copyName.get(a));
-        names.set(a, indexValue);
-        return copyName;
+    private static <T> List<T> swap(List<T> elements, int a, int b) {
+        List<T> copyElements = new ArrayList<>(elements);
+        T indexValue = copyElements.get(b);
+        copyElements.set(b, copyElements.get(a));
+        copyElements.set(a, indexValue);
+        return copyElements;
     }
 
     public static void main(String[] args) {
         List<String> names = Arrays.asList("rahul","pankaj","aman","mohit","nitesh");
         List<Integer> numbers = Arrays.asList(12, 34, 56, 23, 78, 20, 45, 99);
-        System.out.println(swapString(names,0,2));
-        System.out.println(swapNumber(numbers, 0, 2));
+        System.out.println(swap(names,0,2));
+        System.out.println(swap(numbers, 0, 2));
 
     }
 }
